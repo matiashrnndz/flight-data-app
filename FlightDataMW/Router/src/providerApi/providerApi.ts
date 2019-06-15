633c01abdbe6c66e3f7c72c24867e58b149782fd
matiashrnndz <matiashrnndz@gmail.com>
2019-06-14 23:55:02 -0300

    Implementation of FlightDataMW/Router

---
+++ FlightDataMW/Router/src/providerApi/providerApi.ts
@@ -0,0 +1,15 @@
+export default class ProviderApi {
+
+    constructor() {
+
+    }
+
+    async toRoute(data) {
+        console.log("Router - providerApi");
+        console.log(JSON.stringify(data));
+    }
+
+}
+
+
+
